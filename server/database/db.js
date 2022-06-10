bc974eeed0b1d79791f7dc85eddfaeec42293510
Bhaskar Singh <bhaskar.singh144@gmail.com>
2022-06-10 18:28:44 +0530

    DateBase Connceted

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Connection = async () => { 
-    const URL ='mongodb://bhaskarsingh144:#Lucknow123@cluster0-shard-00-00.dxr9a.mongodb.net:27017,cluster0-shard-00-01.dxr9a.mongodb.net:27017,cluster0-shard-00-02.dxr9a.mongodb.net:27017/?ssl=true&replicaSet=atlas-25owq6-shard-0&authSource=admin&retryWrites=true&w=majority'
+    const URL ='mongodb://bhaskarsingh144:Lucknow123@general-shard-00-00.dxr9a.mongodb.net:27017,general-shard-00-01.dxr9a.mongodb.net:27017,general-shard-00-02.dxr9a.mongodb.net:27017/?ssl=true&replicaSet=atlas-r28lu1-shard-0&authSource=admin&retryWrites=true&w=majority'
     try
     {
     await mongoose.connect(URL, {useUnifiedTopology :true , useNewUrlParser :true});
